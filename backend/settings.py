0395ed278e7bc6cd5375b11e9d0b74094db49d00
Mike Lapierre <milapier@microsoft.com>
2024-10-17 09:56:29 -0400

    Add Databricks+MSFT logo

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Microsoft+Databricks"
+    title: str = "Databricks documentation chatbot"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Start chatting"
